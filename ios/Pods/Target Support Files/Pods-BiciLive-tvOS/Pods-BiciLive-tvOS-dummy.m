5116312487de84a232143fd0a40029a864a802af
Supera <supera@Superas-iMac.local>
2020-07-09 10:04:24 -0700

    Remove some

--- ios/Pods/Target Support Files/Pods-BiciLive-tvOS/Pods-BiciLive-tvOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BiciLive_tvOS : NSObject
-@end
-@implementation PodsDummy_Pods_BiciLive_tvOS
-@end
--- ios/Pods/Target Support Files/Pods-BiciLive-tvOSTests/Pods-BiciLive-tvOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BiciLive_tvOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_BiciLive_tvOSTests
-@end
