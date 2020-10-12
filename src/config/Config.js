a5692df927b4e6bc7819a37bf1705b9a217197b3
llod <ptfe0310@outlook.com>
2020-10-12 10:20:32 +0200

    Changed Server Address to publish

--- src/config/Config.js
+++ src/config/Config.js
@@ -1,6 +1,7 @@
 const config = {
-  // server: 'https://www.Bicilive.app',
-  server: 'https://biciapp.sepisolutions.com'
+  server: 'https://www.Bicilive.app',
+  //server: 'https://biciapp.sepisolutions.com'
+  //server: 'https://biciapp.sepisolutions.com'
 };
 
 export default config
